e3e06d49302e97906fc6248a033fca74255d5b11
Rodrigo Rodrigues <rodriigo.rc@gmail.com>
2023-04-23 22:44:36 -0300

    Removendo arquivo

--- Modulo01/Semana03/ExerciciosSemana3/ContaBancaria.cs
+++
@@ -1,5 +0,0 @@
-﻿SELECT * FROM "Occurrence" 
-INNER JOIN "OccurrenceLog" ON "OccurrenceLog"."OccurrenceId" = "Occurrence"."Id" 
-INNER JOIN "Account" ON "Account"."Id" = "Occurrence"."AccountId" 
-INNER JOIN "Event" ON "Event"."Id" = "Occurrence"."EventId" 
-WHERE "Occurrence"."Id" = 1125
